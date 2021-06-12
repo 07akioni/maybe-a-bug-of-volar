ba1a1972444bef896e4d8fadd8c1c4ec2556bc02
07akioni <07akioni2@gmail.com>
2021-06-12 19:30:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <NMenu @update:value="handleUpdateMenuValue" :options="menuOptions" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { MenuOption, NCalendar, NMenu } from "naive-ui";
+export default defineComponent({
+  name: "App",
+  components: {
+    NCalendar,
+    NMenu,
+  },
+  setup() {
+    const menuOptions = [
+      {
+        label: "Item1",
+        key: "item1",
+      },
+      {
+        label: "Item2",
+        key: "item2",
+      },
+    ];
+
+    const handleUpdateMenuValue = (key: string, item: MenuOption) => {
+      console.log("key =>", key);
+      console.log("item =>", item);
+    };
+
+    return {
+      menuOptions,
+      handleUpdateMenuValue,
+    };
+  },
+});
+</script>
